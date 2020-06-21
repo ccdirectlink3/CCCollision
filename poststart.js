8e2ac355a927149450a1815c467dc9b1a642df5f
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-06-21 23:09:06 +0300

    upgrade to ccloader 3

--- poststart.js
+++
@@ -1,5 +0,0 @@
-ig.lang.labels.sc.gui.options.headers['mod-collision'] = 'collision';
-ig.lang.labels.sc.gui.options['show-collisions'] = {
-  name: 'Show collision maps',
-  description: 'Show collision maps',
-};
